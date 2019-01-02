096ce0032508362d748587768bed201dffb1b3c8
rx78nt1alex <35973261+rx78nt1alex@users.noreply.github.com>
2019-01-01 21:37:35 -0600

    Update GarageControl.groovy

--- GarageControl.groovy
+++ GarageControl.groovy
@@ -8,14 +8,18 @@
  *
  *  A WebCoRE piston calls the refresh() function to update the door statuses in SmartThings when a state change 
  *  is published from the Particle Sketch.
+ *
+ * Edited 1/1/19 by Nick Cerda
+ * Commented out extra lines referring to opening doors 2 and 3, and the section for open all, making this more 
+ * for those of us with one garage door. 
 */
 
 preferences {
     input name: "deviceId", type: "text", title: "Device ID", required: true      // Photon device ID from build.particle.io (you'll put this in the settings) 
     input name: "token", type: "password", title: "Access Token", required: true  // Photon super secret access token you get from build.particle.io
     input name: "g1Status", type: "text", title: "Garage 1 Status Variable Name", required: true, defaultValue: "g1Status"  // These three lines define the name of the variable
-    input name: "g2Status", type: "text", title: "Garage 2 Status Variable Name", required: true, defaultValue: "g2Status"  // that you're looking for from your Particle
-    input name: "g3Status", type: "text", title: "Garage 3 Status Variable Name", required: true, defaultValue: "g3Status"  // sketch.
+ //    input name: "g2Status", type: "text", title: "Garage 2 Status Variable Name", required: true, defaultValue: "g2Status"  // that you're looking for from your Particle
+    // input name: "g3Status", type: "text", title: "Garage 3 Status Variable Name", required: true, defaultValue: "g3Status"  // sketch.
     }
 
 metadata {
@@ -29,44 +33,44 @@ metadata {
         capability "Refresh"
         
         attribute "g1Status", "string"  // Defines a container to hold the state of your garage door
-        attribute "g2Status", "string"
-        attribute "g3Status", "string"
+ //        attribute "g2Status", "string"
+ //        attribute "g3Status", "string"
         
         command "garage1"  // Defines a command to trigger the garage door we want opened
-		command "garage2"
-		command "garage3"
-        command "openAll"
-        command "closeAll"
+ //		command "garage2"
+ //		command "garage3"
+        command "open"
+        command "close"
     }
 
     tiles(scale: 2) {
     	// Defines tiles that will appear on the device page
    		standardTile("g1Status", "device.g1Status", width: 6, height: 2, inactiveLabel: false, decoration: "flat") {
-				state "Closed", label: "Left", action: "garage1", icon: "st.doors.garage.garage-closed", backgroundColor: "#83ED5B"
-				state "Open", label: "Left", action: "garage1", icon: "st.doors.garage.garage-open", backgroundColor: "#FF5733"
+				state "Closed", label: "Closed", action: "garage1", icon: "st.doors.garage.garage-closed", backgroundColor: "#83ED5B"
+				state "Open", label: "Open", action: "garage1", icon: "st.doors.garage.garage-open", backgroundColor: "#FF5733"
 		}
-    	standardTile("g2Status", "device.g2Status", width: 6, height: 2, inactiveLabel: false, decoration: "flat") {
-				state "Closed", label: "Middle", action: "garage2", icon: "st.doors.garage.garage-closed", backgroundColor: "#83ED5B"
-				state "Open", label: "Middle", action: "garage2", icon: "st.doors.garage.garage-open", backgroundColor: "#FF5733"
-		}
-        standardTile("g3Status", "device.g3Status", width: 6, height: 2, inactiveLabel: false, decoration: "flat") {
-				state "Closed", label: "Right", action: "garage3", icon: "st.doors.garage.garage-closed", backgroundColor: "#83ED5B"
-				state "Open", label: "Right", action: "garage3", icon: "st.doors.garage.garage-open", backgroundColor: "#FF5733"
-		}
+ //    	standardTile("g2Status", "device.g2Status", width: 6, height: 2, inactiveLabel: false, decoration: "flat") {
+ //				state "Closed", label: "Middle", action: "garage2", icon: "st.doors.garage.garage-closed", backgroundColor: "#83ED5B"
+ //				state "Open", label: "Middle", action: "garage2", icon: "st.doors.garage.garage-open", backgroundColor: "#FF5733"
+ //		}
+ //        standardTile("g3Status", "device.g3Status", width: 6, height: 2, inactiveLabel: false, decoration: "flat") {
+ //				state "Closed", label: "Right", action: "garage3", icon: "st.doors.garage.garage-closed", backgroundColor: "#83ED5B"
+ //				state "Open", label: "Right", action: "garage3", icon: "st.doors.garage.garage-open", backgroundColor: "#FF5733"
+ //		}
    
-        standardTile("openAll", "device.openAll", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
-				state "Closed", label: "OPEN ALL", action: "openAll", icon: "", backgroundColor: "#BABFC6"
-				state "Open", label: "OPEN ALL", action: "openAll", icon: "", backgroundColor: "#BABFC6"
-		}
-        standardTile("closeAll", "device.closeAll", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
-				state "Closed", label: "CLOSE ALL", action: "closeAll", icon: "", backgroundColor: "#BABFC6"
-				state "Open", label: "CLOSE ALL", action: "closeAll", icon: "", backgroundColor: "#BABFC6"
-		}
+ //        standardTile("open", "device.open", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
+ //				state "Closed", label: "OPEN", action: "open", icon: "", backgroundColor: "#BABFC6"
+ //				state "Open", label: "OPEN", action: "open", icon: "", backgroundColor: "#BABFC6"
+ //		}
+ //        standardTile("close", "device.close", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
+ //				state "Closed", label: "CLOSE", action: "close", icon: "", backgroundColor: "#BABFC6"
+ //				state "Open", label: "CLOSE", action: "close", icon: "", backgroundColor: "#BABFC6"
+ //		}
         standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
             state "default", action:"refresh.refresh", icon:"st.secondary.refresh"
         }
 
-		main (["g3Status"])  // This is the tile that will show up on the device's preview.  It's my right garage door in this case.
+		main (["g1Status"])  // This is the tile that will show up on the device's preview.  It's my right garage door in this case.
     }
 
 }
@@ -77,25 +81,25 @@ def updated() {
 	initialize()  // I have no idea what this calls.  It isn't in this DH!
     log.debug "Updated !"
     state.g1Status = 1
-    state.g2Status = 1
-    state.g3Status = 1
+ //    state.g2Status = 1
+ //    state.g3Status = 1
     log.debug "device.g1Status: ${device.g1Status}"
-    log.debug "device.g2Status: ${device.g2Status}"
-    log.debug "device.g3Status: ${device.g3Status}"
+ //    log.debug "device.g2Status: ${device.g2Status}"
+ //    log.debug "device.g3Status: ${device.g3Status}"
 }
 
 def poll() {  // Polls for the status of the garage doors
     log.debug "Executing 'poll'"
     getStatus1()
-    getStatus2()
-    getStatus3()
+ //    getStatus2()
+ //    getStatus3()
 }
 
 def refresh() {  // Calls the garage status functions to refresh them upon refresh button press
     log.debug "Executing 'refresh'"
     getStatus1()
-    getStatus2()
-    getStatus3()
+ //    getStatus2()
+ //    getStatus3()
 }
 
 def garage1() {  // This is what happens when tile g1Status is pushed.
@@ -103,25 +107,25 @@ def garage1() {  // This is what happens when tile g1Status is pushed.
 	put '1'  // Calls put(GarageRelay) and sends it a "1" which tells the Particle sketch to run GarageRelay with command == 1
 }
 
-def garage2() {  // This is what happens when tile g2Status is pushed.
-	log.debug "Executing 'garage2'"
-	put '2'
-}
+ //def garage2() {  // This is what happens when tile g2Status is pushed.
+ //	log.debug "Executing 'garage2'"
+ //	put '2'
+ //}
 
-def garage3() {  // This is what happens when tile g3Status is pushed.
-	log.debug "Executing 'garage3'"
-	put '3'
-}
+ //def garage3() {  // This is what happens when tile g3Status is pushed.
+ //	log.debug "Executing 'garage3'"
+ //	put '3'
+ //}
 
-def openAll() {  // This is what happens when tile openAll is pushed.
-	log.debug "Executing 'openAll'"
-	put 'openAll'
-}
+ //def openAll() {  // This is what happens when tile openAll is pushed.
+ //	log.debug "Executing 'openAll'"
+ //	put 'openAll'
+ //}
 
-def closeAll() {  // This is what happens when tile closeAll is pushed.
-	log.debug "Executing 'closeAll'"
-	put 'closeAll'
-}
+ //def closeAll() {  // This is what happens when tile closeAll is pushed.
+ //	log.debug "Executing 'closeAll'"
+ //	put 'closeAll'
+ //}
 
 // no clue what this does or if its needed
 def parse(String description) {
@@ -139,26 +143,6 @@ private getStatus1() {
     }
 
     httpGet("https://api.particle.io/v1/devices/${deviceId}/${g1Status}?access_token=${token}", closure)
-}
-
-private getStatus2() {
-    def closure = { response ->
-        log.debug "request was successful, $response.data"
-
-        sendEvent(name: "g2Status", value: response.data.result)
-    }
-
-    httpGet("https://api.particle.io/v1/devices/${deviceId}/${g2Status}?access_token=${token}", closure)
-}
-
-private getStatus3() {
-    def closure = { response ->
-        log.debug "request was successful, $response.data"
-
-        sendEvent(name: "g3Status", value: response.data.result)
-    }
-
-    httpGet("https://api.particle.io/v1/devices/${deviceId}/${g3Status}?access_token=${token}", closure)
 }
 
 // How we push commands from the device handler to the Particle
